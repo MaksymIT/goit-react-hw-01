1b5fac13eba6a2885dec3cd7efdc65aa7508cb94
Maksym <121174066+MaksymIT@users.noreply.github.com>
2025-01-17 00:38:49 +0100

    01

---
+++ src/components/WelkomeSection/WelkomeSection.jsx
@@ -0,0 +1,10 @@
+
+const WelkomeSection = () => {
+
+  return (<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias laboriosam non optio, dolore voluptas veniam, eum repellendus ab reiciendis
+    odit tenetur temporibus? Autem at dolorem totam doloremque quisquam. Accusantium, inventore.
+  </p>);
+
+};
+
+export default WelkomeSection;
